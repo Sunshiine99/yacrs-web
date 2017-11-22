3381341e30eb165155c23eca53dd3df5517dc664
David Southgate <d@davidsouthgate.co.uk>
2017-11-22 10:47:33 +0000

    Fixed user permissions not being loaded from database (in the case where permissions have been overrided from the LDAP data)

--- web/src/classes/Login/Login.php
+++ web/src/classes/Login/Login.php
@@ -5,6 +5,22 @@ class Login
 
     public static function checkLogin($username, $password, $type="ldap") {
         $login = LoginTypeFactory::create($type);
-        return $login::checkLogin($username, $password);
+
+        $uinfo = $login::checkLogin($username, $password);
+
+
+        if(!$uinfo)
+            return false;
+
+        // Load the user details from the database
+        $databaseUser = DatabaseUser::retrieveByUsername($username);
+
+        // If database has user, override session creator and admin privileges
+        if($databaseUser) {
+            $uinfo["isAdmin"] = $databaseUser->isAdmin();
+            $uinfo["sessionCreator"] = $databaseUser->isSessionCreator();
+        }
+
+        return $uinfo;
     }
 }

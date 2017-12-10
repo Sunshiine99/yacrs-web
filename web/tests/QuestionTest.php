33056067209a0782dd7ef9fcf3e6a1effdd78a3e
risto <h.ivanov96@gmail.com>
2017-12-10 15:32:09 +0000

    added unit tests #17

--- web/tests/QuestionTest.php
+++ web/tests/QuestionTest.php
@@ -23,4 +23,42 @@ final class QuestionTest extends TestCase
             $questionArr
         );
     }
+
+    public function testQuestionType(){
+        $questionArr = [];
+        $questionMcq = QuestionFactory::create("mcq", $questionArr);
+        $questionText = QuestionFactory::create("text", $questionArr);
+        $questionLong = QuestionFactory::create("textlong", $questionArr);
+        $this->assertInstanceOf(
+            QuestionMcq::class,
+            $questionMcq
+        );
+        $this->assertInstanceOf(
+            QuestionText::class,
+            $questionText
+        );
+        $this->assertInstanceOf(
+            QuestionTextLong::class,
+            $questionLong
+        );
+    }
+
+    public function testMcq(){
+        $questionArr = [];
+        $questionMcq = new QuestionMcq($questionArr);
+        $questionMcq->addChoice("a");
+        $this->assertEquals(
+            count($questionMcq->getChoices()),
+            1
+        );
+    }
+
+    public function testMcqChoice(){
+        $choice = new QuestionMcqChoice("a", "true");
+        $arr = $choice->toArray();
+        $this->assertEquals(
+            $arr,
+            array("choice" => "a", "correct" => true)
+        );
+    }
 }

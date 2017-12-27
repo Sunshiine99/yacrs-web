5c8a041a399f00b3dc0965243a232b82a89d4ff2
David Southgate <d@davidsouthgate.co.uk>
2017-12-27 23:41:56 +0000

    Added CSQ login for TP3 development

---
+++ web/src/classes/Login/LoginType/LoginTypeLdapCsq.php
@@ -0,0 +1,33 @@
+<?php
+
+class LoginTypeLdapCsq extends LoginTypeLdap
+{
+
+    /**
+     * Checks login username and password
+     * @param $username
+     * @param $password
+     * @param array $config
+     * @return User|null
+     */
+    public static function checkLogin($username, $password, $config = []) {
+
+        // Perform LDAP login
+        $user = parent::checkLogin($username, $password, $config);
+
+        // Force all CSQ members to have admin and teacher permissions
+        $username = strtolower($username);
+        switch($username) {
+            case "2262645c";    // Chase
+            case "2205747i";    // Hristo
+            case "2141683m";    // Michael
+            case "2198207s";    // David
+            case "2036909a";    // Nora
+                $user->setIsSessionCreator(true);
+                $user->setIsAdmin(true);
+                break;
+        }
+
+        return $user;
+    }
+}

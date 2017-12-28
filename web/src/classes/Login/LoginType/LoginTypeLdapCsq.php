0c893e088f7628dd0cdde8735740cbdb944bfa26
David Southgate <d@davidsouthgate.co.uk>
2017-12-28 00:06:16 +0000

    Fixed CSQ login

--- web/src/classes/Login/LoginType/LoginTypeLdapCsq.php
+++ web/src/classes/Login/LoginType/LoginTypeLdapCsq.php
@@ -15,17 +15,21 @@ class LoginTypeLdapCsq extends LoginTypeLdap
         // Perform LDAP login
         $user = parent::checkLogin($username, $password, $config);
 
-        // Force all CSQ members to have admin and teacher permissions
-        $username = strtolower($username);
-        switch($username) {
-            case "2262645c";    // Chase
-            case "2205747i";    // Hristo
-            case "2141683m";    // Michael
-            case "2198207s";    // David
-            case "2036909a";    // Nora
-                $user->setIsSessionCreator(true);
-                $user->setIsAdmin(true);
-                break;
+        // If login was valid
+        if($user) {
+
+            // Force all CSQ members to have admin and teacher permissions
+            $username = strtolower($username);
+            switch($username) {
+                case "2262645c";    // Chase
+                case "2205747i";    // Hristo
+                case "2141683m";    // Michael
+                case "2198207s";    // David
+                case "2036909a";    // Nora
+                    $user->setIsSessionCreator(true);
+                    $user->setIsAdmin(true);
+                    break;
+            }
         }
 
         return $user;

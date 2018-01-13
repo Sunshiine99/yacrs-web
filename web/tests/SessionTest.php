ee6b4e8b5b9bcbc958c42a5140b8ef0f6cee4356
risto <h.ivanov96@gmail.com>
2018-01-13 18:21:00 +0000

    fixed unit tests #17

--- web/tests/SessionTest.php
+++ web/tests/SessionTest.php
@@ -8,7 +8,7 @@ final class SessionTest extends TestCase{
 
     public function testSessionArray(){
         $arr = [];
-        $arr["sessionID"] = 3;
+        $arr["sessionIdentifier"] = null;
         $arr["owner"] = "a";
         $arr["title"] = "test";
         $arr["courseID"] = "aa";

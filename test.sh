1fd48369cb8def8a21008c1b663398d96bb07abb
David Southgate <d@davidsouthgate.co.uk>
2018-01-27 00:07:05 +0000

    Updated test script

--- test.sh
+++ test.sh
@@ -14,11 +14,11 @@ docker-compose rm -f
 docker-compose build
 
 # Run docker compose in test config
-docker-compose -f docker-compose.yml -f docker-compose.phpunit.yml up --timeout 1 --no-build
+docker-compose -f docker-compose.yml -f docker-compose.phpunit.yml up --abort-on-container-exit
 
 # Stop the container and output the logs of the test
-docker-compose stop
-docker-compose logs web
+#docker-compose stop
+#docker-compose logs web
 
 # Gets exit code from docker
 exitCode=$(docker-compose ps -q | xargs docker inspect -f '{{ .Name }} exited with status {{ .State.ExitCode }}' | grep /yacrsweb_web_1 | grep "status [0-9]*" -oh | grep "[0-9]*" -oh)

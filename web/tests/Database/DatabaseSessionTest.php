5320e7128f2fd98bc5112d3b92749f71d9561879
risto <h.ivanov96@gmail.com>
2018-02-03 19:37:39 +0000

    added more tests #17

--- web/tests/Database/DatabaseSessionTest.php
+++ web/tests/Database/DatabaseSessionTest.php
@@ -21,11 +21,11 @@ final class DatabaseSessionTest extends TestCase
 
         $array = [];
         $array["title"] = "name";
-        $array["ownerID"] = $user->getId();
+        $array["owner"] = $user->getId();
         $session = new Session($array);
 
         $sessionIdentifier = DatabaseSession::insert($session, $mysqli);
-        //$this->assertNotNull($sessionIdentifier);
+        $this->assertNotNull($sessionIdentifier);
     }
 
     public function testLoad(){
@@ -40,14 +40,63 @@ final class DatabaseSessionTest extends TestCase
 
         $array = [];
         $array["title"] = "name";
-        $array["ownerID"] = $user->getId();
+        $array["owner"] = $user->getId();
         $session = new Session($array);
 
         $sessionIdentifier = DatabaseSession::insert($session, $mysqli);
 
-        /*$this->assertNotNull(DatabaseSession::loadSession
+        $this->assertNotNull(DatabaseSession::loadSession
         (DatabaseSessionIdentifier::loadSessionID
-        ($sessionIdentifier, $mysqli), $mysqli));*/
+        ($sessionIdentifier, $mysqli), $mysqli));
+
+        $this->assertNotNull(DatabaseSession::loadUserSessions($user->getId(), $mysqli));
     }
 
+    public function testUpdate(){
+
+        global $config;
+
+        // Connect to the database
+        $mysqli = TestHelper::databaseConnect($config);
+
+        // Create a new user
+        $user = TestHelper::userSessionCreator($config, $mysqli);
+
+        $array = [];
+        $array["title"] = "name";
+        $array["owner"] = $user->getId();
+        $session = new Session($array);
+
+        $sessionIdentifier = DatabaseSession::insert($session, $mysqli);
+        $session = DatabaseSessionIdentifier::loadSession($sessionIdentifier, $mysqli);
+        $session->setTitle("test");
+
+        DatabaseSession::update($session, $mysqli);
+
+        $this->assertEquals(
+            DatabaseSession::loadSession
+            (DatabaseSessionIdentifier::loadSessionID
+            ($sessionIdentifier, $mysqli), $mysqli)->getTitle(),
+            "test"
+        );
+    }
+
+    public function testDelete(){
+        global $config;
+
+        // Connect to the database
+        $mysqli = TestHelper::databaseConnect($config);
+
+        // Create a new user
+        $user = TestHelper::userSessionCreator($config, $mysqli);
+
+        $array = [];
+        $array["title"] = "name";
+        $array["owner"] = $user->getId();
+        $session = new Session($array);
+
+        $sessionIdentifier = DatabaseSession::insert($session, $mysqli);
+
+        $this->assertTrue(DatabaseSessionIdentifier::delete($sessionIdentifier, $mysqli));
+    }
 }

9026d9e8f7e86e776e647587cae815c9a7d0a485
Nor Al Bagdadi <i-l-y_93@hotmail.com>
2018-02-07 16:22:51 +0000

    icons added #63

--- web/src/templates/session/edit/class.php
+++ web/src/templates/session/edit/class.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
     <title>Live View</title>
 
@@ -20,8 +22,28 @@
 </head>
 
 <body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-1">
+            <i class="fa fa-angle-double-left"></i>
+        </div>
+        <div class="col-md-1">
+            <i class="fa fa-angle-double-right"></i>
+        </div>
+        <div class="col-md-1">
+            <i class="fa fa-power-off"></i>
+        </div>
+        <div class="col-md-1">
+            <i class="fa fa-chart-bar"></i>
+        </div>
+        <div class="col-md-1">
+            <i class="fa fa-chart-pie"></i>
+        </div>
+
+    </div>
     Live View <br>
     <a href="#" onclick="exitClassMode()">BACK</a>
+</div>
 </body>
 
 </html>

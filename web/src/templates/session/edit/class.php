bec33fde056b4f44787309533f84f1b8c8d467c4
Nor Al Bagdadi <i-l-y_93@hotmail.com>
2018-02-07 14:50:55 +0000

    live view initial commit #63

--- web/src/templates/session/edit/class.php
+++ web/src/templates/session/edit/class.php
@@ -1,2 +1,21 @@
-CLASS MODE<br>
-<a href="<?=$config["baseUrl"]?>session/<?=$this->e($session->getSessionIdentifier())?>/edit/">Go back</a>
+Live View <br>
+<a href="<?=$config["baseUrl"]?>session/<?=$this->e($session->getSessionIdentifier())?>/edit/">Back</a>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Live View</title>
+</head>
+
+<body>
+
+</body>
+
+</html>

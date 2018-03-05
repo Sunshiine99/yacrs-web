d9b826d5c8e0473158548c66d5e6431bb0a9404f
David Southgate <d@davidsouthgate.co.uk>
2018-03-05 00:27:01 +0000

    Minor download page changes

--- web/src/templates/download.php
+++ web/src/templates/download.php
@@ -22,7 +22,7 @@ $this->layout("template",
     <div class="container">
         <h1 class="display-3">Download YACRS</h1>
         <p class="lead">
-            Latest Version: v2.0.0
+            Latest Version: v2.0.0rc1
         </p>
     </div>
 </div>
@@ -31,19 +31,19 @@ $this->layout("template",
 <div class="row">
     <div class="offset-sm-2 col-sm-8 row">
         <div class="col-sm-4">
-            <a class="btn btn-primary btn-lg width-full download" target="_blank" href="https://www.dropbox.com/sh/w2347x3h7i0h6wb/AAANRFttnqcHKXWs7uk0mc1Ka?dl=0">
+            <a class="btn btn-primary btn-lg width-full download" target="_blank" href="https://www.dropbox.com/sh/w2347x3h7i0h6wb/AAANRFttnqcHKXWs7uk0mc1Ka?dl=0&lst=">
                 <i class="fa fa-windows icon" aria-hidden="true"></i>
                 <span class="os">Windows</span>
             </a>
         </div>
         <div class="col-sm-4">
-            <a class="btn btn-primary btn-lg width-full download" target="_blank" href="https://www.dropbox.com/sh/1ptq1dyj5y15oxw/AAASIItlXGfDVBrO93FKOaOya?dl=0">
+            <a class="btn btn-primary btn-lg width-full download" target="_blank" href="https://www.dropbox.com/sh/1ptq1dyj5y15oxw/AAASIItlXGfDVBrO93FKOaOya?dl=0&lst=">
                 <i class="fa fa-apple icon" aria-hidden="true"></i>
                 <span class="os">macOS</span>
             </a>
         </div>
         <div class="col-sm-4">
-            <a class="btn btn-primary btn-lg width-full download" target="_blank" href="https://www.dropbox.com/sh/1xaatvmciq0uq2s/AAD-UkL_05qdDuhAKA_XZwxia?dl=0">
+            <a class="btn btn-primary btn-lg width-full download" target="_blank" href="https://www.dropbox.com/sh/1xaatvmciq0uq2s/AAD-UkL_05qdDuhAKA_XZwxia?dl=0&lst=">
                 <i class="fa fa-linux icon" aria-hidden="true"></i>
                 <span class="os">Linux</span>
             </a>
